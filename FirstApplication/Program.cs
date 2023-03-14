996995f457c5c93bcc8abc2aab39b5af03f3acb6
Pavel <Pavel.romon@gmail.com>
2023-03-14 08:57:45 +0500

    Добавьте файлы проекта.

---
+++ FirstApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; // Подключенное пространство имен
+using System.Threading.Channels;
+
+namespace FirstApplication.ConsoleApp // Объявление пространства имен для данного кода
+{
+    class Program // Объявление класса Program
+    {
+        static void Main(string[] args) // Объявление метода Main
+        {
+            string MyName;
+            MyName = "Pavel";
+            Console.WriteLine(MyName);
+
+        }
+    }
+}
