8a88c5f2b8b3916126c166d8b2cf180b0d6520f3
Pavel Konstantinov <Pavel.romon@gmail.com>
2023-04-06 22:16:03 +0500

    Unit 4.1.17 2.0

--- FirstApplication/Program.cs
+++ FirstApplication/Program.cs
@@ -7,11 +7,32 @@ namespace FirstApplication.ConsoleApp // Объявление пространс
     {
         static void Main(string[] args) // Объявление метода Main
         {
-            var inv = true;
-            var result = !inv;
-            Console.WriteLine(result);
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
+            
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
 
+                Console.WriteLine("Your color is red!");
+            }
+            
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
 
+                Console.WriteLine("Your color is green!");
+            }
+
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
 
         }
     }
